efac0fd1f732f9d0f3cc26f3fa5d864c5cfe7f55
Nisha Chaube <nisha@Nishas-MacBook-Pro.local>
2020-07-28 01:38:01 -0400

    Updates

---
+++ precache-manifest.5172e576e054dd09e15ad0cff846bf37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d0b6756ed9be3f4ad90c1d8289f858",
+    "url": "/React-Samples/index.html"
+  },
+  {
+    "revision": "04ce225a37fa773d45bc",
+    "url": "/React-Samples/static/css/main.c5c37a0a.chunk.css"
+  },
+  {
+    "revision": "250f032249a97c1eb92c",
+    "url": "/React-Samples/static/js/2.f0516e72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Samples/static/js/2.f0516e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ce225a37fa773d45bc",
+    "url": "/React-Samples/static/js/main.09c7bf4e.chunk.js"
+  },
+  {
+    "revision": "bff68a1c2a6ec9529c6b",
+    "url": "/React-Samples/static/js/runtime-main.f8ce0fec.js"
+  }
+]);
